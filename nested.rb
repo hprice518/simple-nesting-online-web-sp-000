119b37bf74fe7a10ad09753c107944bd2e097d4b
Harrison Price <hprice518@gmail.com>
2021-03-27 19:58:40 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -88,7 +88,7 @@ def adding_matz
      }
 
    programmer_hash[
-     :yukihiro_matsumoto = {:known_for => "Ruby",
+     :yukihiro_matsumoto => {:known_for => "Ruby",
         :languages => ["LISP", "C"]}
     ]
   
@@ -113,8 +113,11 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
      
+     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     
+     programmer_hash
      
 end
 
@@ -135,4 +138,8 @@ def adding_to_dennis
         }
      }
 
+
+programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+
+programmer_hash
 end
